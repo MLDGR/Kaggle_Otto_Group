1fb01dcc24384e0d52bc4befa39ac9442636436e
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-05-20 14:04:00 +0200

    nnet R

---
+++ R/nnet.R
@@ -0,0 +1,15 @@
+#Load dependency
+library(nnet)
+
+# set seed
+set.seed(1337)
+
+source("Load-Transform-Data.R")
+
+# fit and predict
+fit<-nnet(target ~ ., train, size = 9, rang = 0.1, decay = 5e-4, maxit = 500)
+predicted<-as.data.frame(predict(fit,test,type="raw"))
+
+id<-1:dim(test)[1]
+output<-cbind(id,predicted)
+write.csv(output,file="neural_network_1.csv",row.names=FALSE)
